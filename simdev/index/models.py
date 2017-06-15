f131ce0e09325d19dee1ca56babec8f49fbe608f
Yacow <midnight.howlet@gmail.com>
2017-06-15 12:10:50 +0300

    index

---
+++ simdev/index/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Record(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    scores = models.BigIntegerField(default=0)
+    data = models.TextField(null=True)
